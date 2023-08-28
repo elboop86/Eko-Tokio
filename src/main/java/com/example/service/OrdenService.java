0a690025c0505e570d6555c92b0223702c0213ef
Javier Medina <javi86medina@hotmail.com>
2023-08-28 16:18:16 +0200

    mértodo para generar el numero de la orden.

--- src/main/java/com/example/service/OrdenService.java
+++ src/main/java/com/example/service/OrdenService.java
@@ -2,6 +2,9 @@ package com.example.service;
 
 import com.example.model.Orden;
 
+import java.util.List;
+
 public interface OrdenService {
+    List<Orden> findAll();
     Orden save(Orden orden);
 }
--- src/main/java/com/example/service/OrdenServiceImpl.java
+++ src/main/java/com/example/service/OrdenServiceImpl.java
@@ -5,10 +5,49 @@ import com.example.repository.OrdenRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.criteria.CriteriaBuilder;
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class OrdenServiceImpl implements OrdenService{
     @Autowired
     private OrdenRepository ordenRepository;
+
+    @Override
+    public List<Orden> findAll() {
+        return ordenRepository.findAll();
+    }
+
+    public String generarNumeroOrden() {
+        int numero= 0;
+        String numeroConcatenado= "";
+
+        List<Orden> ordenes = findAll();
+
+        List<Integer> numeros = new ArrayList<Integer>();
+        ordenes.stream().forEach(o -> numeros.add(Integer.parseInt(o.getNumero())));
+
+        // La numeración de las ordenes hasta 10.000, se ordena un numero ordenado a mayor a cada orden realizada.
+        if(ordenes.isEmpty()) {
+            numero= 1;
+        } else {
+            numero = numeros.stream().max(Integer::compare).get();
+            numero++;
+        }
+
+        if(numero<10) {
+            numeroConcatenado = "000000000" + String.valueOf(numero);
+        } else if(numero<100) {
+            numeroConcatenado= "00000000" + String.valueOf(numero);
+        } else if (numero<1000) {
+            numeroConcatenado = "0000000" + String.valueOf(numero);
+        } else if (numero<10000) {
+            numeroConcatenado = "000000" + String.valueOf(numero);
+        }
+        return "numeroConcatenado";
+    }
+
     @Override
     public Orden save(Orden orden) {
         return ordenRepository.save(orden);
