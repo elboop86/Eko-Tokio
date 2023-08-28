02175c1e32f3c4285de1089125da2b907fd22a62
Javier Medina <javi86medina@hotmail.com>
2023-08-28 19:40:37 +0200

    Obtener usuario login y session.

--- src/main/java/com/example/controller/UsuarioController.java
+++ src/main/java/com/example/controller/UsuarioController.java
@@ -2,6 +2,7 @@ package com.example.controller;
 
 import com.example.model.Usuario;
 import com.example.service.UsuarioService;
+import com.mysql.cj.x.protobuf.MysqlxCursor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpSession;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/usuario")
@@ -29,4 +33,31 @@ public class UsuarioController {
         usuarioService.save(usuario);
         return "redirect:/";
     }
+    // usuario Login
+    @GetMapping("/login")
+    public String login() {
+        return "usuario/login";
+    }
+    @PostMapping("/acceder")
+    public String acceder(Usuario usuario, HttpSession session) {
+        logger.info("Accesos: {}", usuario);
+
+        // Obtener usuario que tenga este email.
+        Optional<Usuario> user = usuarioService.findByEmail(usuario.getEmail());
+        logger.info("Usuario de base de datos: {}", user.get());
+
+        // si esta presente el usuario se crea la sesion.
+        if(user.isPresent()) {
+            session.setAttribute("idusuario", user.get().getId());
+            if(user.get().getTipo().equals("ADMIN")) {
+                return "redirect:/administrador";
+            }else {
+                return "redirect:/";
+            }
+        } else {
+            logger.info("Usuario no existe");
+        }
+        return "redirect:/";
+    }
+
 }
--- src/main/java/com/example/service/UsuarioService.java
+++ src/main/java/com/example/service/UsuarioService.java
@@ -7,4 +7,5 @@ import java.util.Optional;
 public interface UsuarioService {
     Optional<Usuario> findById(Integer id);
     Usuario save(Usuario usuario);
+    Optional<Usuario> findByEmail(String email);
 }
